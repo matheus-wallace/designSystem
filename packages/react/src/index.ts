f48c24998b500cc9ea9029ce67a1ce1b8599acae
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-06-28 17:26:55 -0300

    chore: add react

--- packages/react/src/index.ts
+++
@@ -1,4 +0,0 @@
-console.log('ok');
-import { colors } from '@matheus/tokens';
-
-console.log(colors);
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@matheus/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
